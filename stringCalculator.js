ca0972b6b2005ecc43dea59096b4d2f49074f407
yashkumarsaurabh <65075425+yashkumarsaurabh@users.noreply.github.com>
2024-07-26 13:08:23 +0530

    added test and function for single string and comma separated string

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,11 @@
 class StringCalculator {
     add(stringOfNumbers) {
       if (!stringOfNumbers) return 0;
+
+    let delimiter = /,/;
+    const numbers = stringOfNumbers.split(delimiter).map(Number);
+
+    return numbers.reduce((sum, num) => sum + num, 0);
     }
 }
 
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -5,4 +5,12 @@ describe('stringCalculator', () => {
   it('should return 0 for an empty string', () => {
     expect(stringCalculator.add("")).to.equal(0);
   });
+
+  it('should return 1 for "1"', () => {
+    expect(stringCalculator.add("1")).to.equal(1);
+  });
+
+  it('should return 6 for "1,5"', () => {
+    expect(stringCalculator.add("1,5")).to.equal(6);
+  });
 });
